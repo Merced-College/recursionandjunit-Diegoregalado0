5ac7befae44fa80b423fcd680c446a0bbbb53e0e
Diego Regalado <dregalado3@ucmerced.edu>
2025-07-16 22:56:58 +0000

    recursion problems class with count8 method

---
+++ RecursionProblems.java
@@ -0,0 +1,11 @@
+public class RecursionProblems {
+
+    public static int count8(int n){
+        if (n == 0) return 0;
+        int last = n % 10;
+        int next = (n / 10) % 10;
+
+        if (last == 8){
+            if (next =)
+        }
+    }
